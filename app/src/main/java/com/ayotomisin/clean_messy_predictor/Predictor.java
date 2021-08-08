3ce89c3e5eadf3f43c9817f143b9337ce0d96e58
king-tomi <tayodabo195@stu.ui.edu.ng>
2021-08-08 12:29:06 +0100

    created for ui for the app

---
+++ app/src/main/java/com/ayotomisin/clean_messy_predictor/Predictor.java
@@ -0,0 +1,10 @@
+package com.ayotomisin.clean_messy_predictor;
+
+import android.opengl.Matrix;
+
+public class Predictor {
+
+    public static String predict (Matrix image) {
+        return "";
+    }
+}
